8764e8525dbe6f6955b83c95f0c5211420a2fac8
ananth <uananth.1703@gmail.com>
2022-03-12 17:44:37 +0530

    initial commit

---
+++ src/JavaPrograms.java
@@ -0,0 +1,9 @@
+import looping.NumbersOperation;
+
+public class JavaPrograms {
+    public static void main(String[] args){
+        System.out.println("WELCOME TO NUMBERS OPERATION USING LOOPS");
+        NumbersOperation numbersOperation = new NumbersOperation();
+    }
+
+}
