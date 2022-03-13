a70216e13bebcf3c20816cddc1b17b05dd80333e
ananth <uananth.1703@gmail.com>
2022-03-13 22:05:59 +0530

    today's work 1.printing a given number for 5 times 2.printing 1 to N numbers 3.printing odd numbers from 1 to N 4.printing even numbers from 1 to N 5.printing addition of first N numbers

--- src/JavaPrograms.java
+++ src/JavaPrograms.java
@@ -3,7 +3,13 @@ import looping.NumbersOperation;
 public class JavaPrograms {
     public static void main(String[] args){
         System.out.println("WELCOME TO NUMBERS OPERATION USING LOOPS");
+
         NumbersOperation numbersOperation = new NumbersOperation();
+        numbersOperation.printingNumberFor5Times(8);
+        numbersOperation.printing1ToN(5);
+        numbersOperation.printingOddNumbers(10);
+        numbersOperation.printingEvenNumbers(10);
+        numbersOperation.printingAdditionOfFirstNNumbers(4);
     }
 
 }
--- src/looping/NumbersOperation.java
+++ src/looping/NumbersOperation.java
@@ -1,4 +1,38 @@
 package looping;
 
 public class NumbersOperation {
+    public void printingNumberFor5Times(int no){
+        System.out.println("Printing a number for 5 Times");
+        for (int number = 1; number<=5; number++) {
+            System.out.println(no);
+        }
+    }
+    public void printing1ToN(int no){
+        System.out.println("Printing 1 to N numbers");
+        for (int number = 1; number<=no; number++){
+            System.out.println(number);
+        }
+    }
+    public void printingOddNumbers(int no){
+        System.out.println("Printing odd numbers from 1 to N numbers");
+        for (int number = 1; number<=10; number++){
+            System.out.println(number);
+            number++;
+        }
+    }
+    public void printingEvenNumbers(int no){
+        System.out.println("Printing even numbers from 1 to N numbers");
+        for (int number = 2; number<=no; number++ ){
+            System.out.println(number);
+            number++;
+        }
+    }
+    public void printingAdditionOfFirstNNumbers(int no){
+        System.out.println("Printing Addition Of First N Numbers");
+        int total=0;
+        for (int number = 1; number<=no; number++){
+            total = total+number;
+        }
+        System.out.println(total);
+    }
 }
